1d5d71f3d72e136ac9a230d2e25d0335f1dc8bb2
Nick D'Oria <17dorin@gmail.com>
2021-03-01 16:38:13 -0500

    Finished lab

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RedditAPILab.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedditAPILab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private RedditDAL rdal = new RedditDAL();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DisplayReddit(string sub)
+        {
+            string url = $@"https://www.reddit.com/r/{sub}/.json";
+            Reddit r = rdal.ConvertToReddit(url);
+            return View(r);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/RedditDAL.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace RedditAPILab.Models
+{
+    public class RedditDAL
+    {
+        public string GetData(string url)
+        {
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            string json = rd.ReadToEnd();
+
+            return json;
+        }
+
+        public Reddit ConvertToReddit(string url)
+        {
+            string redditData = GetData(url);
+            Reddit r = JsonConvert.DeserializeObject<Reddit>(redditData);
+
+            return r;
+        }
+    }
+}
